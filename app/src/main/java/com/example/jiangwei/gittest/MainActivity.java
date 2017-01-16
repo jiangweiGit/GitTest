2d9e9691925d594708b838316a29b7304afe17ee
jiangwei <jiagnwei@huimv.com>
2017-01-16 16:55:29 +0800

    Merge branch 'master' into GitTestBranch1
    # Conflicts:
    #	app/src/main/java/com/example/jiangwei/gittest/MainActivity.java

--- app/src/main/java/com/example/jiangwei/gittest/MainActivity.java
+++ app/src/main/java/com/example/jiangwei/gittest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试aa 分
-        //分支合并
+        //分支合并fdsa
     }
 }
